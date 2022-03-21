55f44c447f0f870ff6e4813f9bb9af0df0e75d67
Aarav Wadaskar <wadaskar.aarav@gmail.com>
2022-03-21 18:30:05 +0530

    Add files via upload

---
+++ cd.py
@@ -0,0 +1,6 @@
+a=input("you have won a price of 230000000 dollars,to enter press type a")
+if a="a" 
+print('you chose right lad')
+b=input('but u need to enter ur credit card information first,thats fine isnt it?')
+else:
+    print("f u")
